1c39d2cbebc040aa4162e46a480bc37742b1e085
Alexis Metaireau <alexis@notmyidea.org>
2012-03-23 15:40:15 +0100

    remove tokenlib dependency

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-import os
 from setuptools import setup, find_packages
 
-
-requires = ['cornice', 'mozsvc', 'tokenlib']
-
+requires = ['cornice', 'mozsvc']
 
 setup(name='stokenserver',
       version='0.1',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      entry_points = """\
+      entry_points="""\
       [paste.app_factory]
       main = stokenserver:main
       """,
