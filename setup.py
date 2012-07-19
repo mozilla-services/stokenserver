7ddd03a8bc727d97dd05e155cbf061889d017ba9
Tarek Ziade <tarek@ziade.org>
2012-07-19 20:26:44 +0200

    prep 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 requires = ['cornice', 'mozsvc']
 
 setup(name='stokenserver',
-      version='0.2',
+      version='0.3',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
